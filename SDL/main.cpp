992eecd86646eae7a0b2547b2332895327b01c6c
shawn <jak47042007@163.com>
2015-10-13 22:28:09 -0700

    random permutation 4

--- SDL/main.cpp
+++ SDL/main.cpp
@@ -4,13 +4,18 @@
 //Using SDL, SDL_image, standard IO, math, and strings
 #include <SDL2/SDL.h>
 #include <SDL2_image/SDL_image.h>
-#include <stdio.h>
 #include <string>
-#include <cmath>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <stack>
+#include <unordered_set>
+
+using namespace std;
 
 //Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 1024;
+const int SCREEN_HEIGHT = 768;
 
 //Starts up SDL and creates window
 bool init();
@@ -22,7 +27,7 @@ bool loadMedia();
 void close();
 
 //Loads individual image as texture
-SDL_Texture* loadTexture( std::string path );
+SDL_Texture* loadTexture( string path );
 
 //The window we'll be rendering to
 SDL_Window* gWindow = NULL;
@@ -84,6 +89,15 @@ bool init()
     return success;
 }
 
+bool loadMedia()
+{
+    //Loading success flag
+    bool success = true;
+    
+    //Nothing to load
+    return success;
+}
+
 void close()
 {
     //Destroy window
@@ -124,8 +138,125 @@ SDL_Texture* loadTexture( std::string path )
     return newTexture;
 }
 
+struct Cell {
+    int x;      // position
+    int y;
+    int size;
+    
+    bool up;
+    bool down;
+    bool left;
+    bool right;
+    
+    Cell (int size) {
+        this->size = size;
+        up = down = left = right = false;
+    }
+};
+
+void drawRect(const Cell &r) {
+    
+    int offset = 20;
+    
+    SDL_Rect rect = {r.y + offset, r.x + offset, r.size, r.size};
+	SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0x00, 0xFF );
+	SDL_RenderDrawRect( gRenderer, &rect);
+}
+
+void displayBoard(const vector<vector<Cell>> &board) {
+    for (int i = 0; i < board.size(); i++) {
+        for (int j = 0; j < board[0].size(); j++) {
+            drawRect(board[i][j]);
+        }
+    }
+}
+
+int getRand4() {
+    return rand() % 4;
+}
+
+const int m = 30;              // the number of rows
+const int n = 40;              // the number of columns
+vector<vector<bool>> visited(m, vector<bool>(n, false));
+
+int getRandomLegalDirection(int i, int j, const vector<vector<Cell>> &board) {
+    int count = 0;
+    unordered_set<int>  tried;
+    while (tried.size() < 4) {
+        int rand = getRand4();
+        tried.insert(rand);
+        if (rand == 0) {
+            if (i - 1 >= 0 && !visited[i-1][j]) {
+                return 0;
+            }
+        } else if (rand == 1) {
+            if (i + 1 < m && !visited[i+1][j]) {
+                return 1;
+            }
+        } else if (rand == 2) {
+            if (j - 1 >= 0 && !visited[i][j-1]) {
+                return 2;
+            }
+        } else {    // rand == 3
+            if (j + 1 < n && !visited[i][j+1]) {
+                return 3;
+            }
+        }
+        count++;
+    }
+    // no feasible direction
+    return -1;
+}
+
+vector<int> getRandomPermutation4() {
+    vector<int> ret;
+    unordered_set<int> s;
+    while (s.size() < 4) {
+        int temp = getRand4();
+        s.insert(temp);
+        if (s.find(temp) == s.end()) {
+            ret.push_back(temp);
+        }
+    }
+    return ret;
+}
+
+
+void getPath(vector<vector<Cell>> &board) {
+    stack<Cell> stk;
+    stk.push(board[0][0]);
+    while (!stk.empty()) {
+        Cell temp = stk.top();
+        stk.pop();
+        int x = temp.x;
+        int y = temp.y;
+        visited[x][y] = true;
+        // push all possible move to stack
+        vector<int> sequence = getRandomPermutation4();
+        
+        for (int i = 0; i < 4; i++) {
+            if (sequence[i] == 0) {
+                
+            } else if
+        }
+     }
+}
+
+
 int main( int argc, char* args[] )
 {
+    const int size = 20;
+    // do computation here
+    vector<vector<Cell>> board(m, vector<Cell>(n, Cell(size)));
+    srand((int)time(0));
+    
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            board[i][j].x = i * size;
+            board[i][j].y = j * size;
+        }
+    }
+    
     //Start up SDL and create window
     if( !init() )
     {
@@ -133,38 +264,41 @@ int main( int argc, char* args[] )
     }
     else
     {
-        //Main loop flag
-        bool quit = false;
-        
-        //Event handler
-        SDL_Event e;
-        
-        //While application is running
-        while( !quit )
+        //Load media
+        if( !loadMedia() )
         {
-            //Handle events on queue
-            while( SDL_PollEvent( &e ) != 0 )
+            printf( "Failed to load media!\n" );
+        }
+        else
+        {
+            //Main loop flag
+            bool quit = false;
+            
+            //Event handler
+            SDL_Event e;
+            
+            //While application is running
+            while( !quit )
             {
-                //User requests quit
-                if( e.type == SDL_QUIT )
+                //Handle events on queue
+                while( SDL_PollEvent( &e ) != 0 )
                 {
-                    quit = true;
+                    //User requests quit
+                    if( e.type == SDL_QUIT )
+                    {
+                        quit = true;
+                    }
                 }
+                
+                //Clear screen
+                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+                SDL_RenderClear( gRenderer );
+                
+                displayBoard(board);
+                
+                //Update screen
+                SDL_RenderPresent( gRenderer );
             }
-            
-            SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-            SDL_RenderClear( gRenderer );
-            
-            SDL_SetRenderDrawColor(gRenderer, 255, 0, 0, 255);
-            SDL_Rect rectangle;
-            rectangle.x = 0;
-            rectangle.y = 0;
-            rectangle.w = 50;
-            rectangle.h = 50;
-            SDL_RenderFillRect(gRenderer, &rectangle);
-            
-            //Update screen
-            SDL_RenderPresent( gRenderer );
         }
     }
     
